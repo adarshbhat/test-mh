62f226a807128706c82a6f25e881017698813ff0
Adarsh Bhat <adarsh.bhat@imanage.com>
2016-07-23 12:14:16 -0700

    Removed insurance information

--- contact.php
+++ contact.php
@@ -90,8 +90,6 @@ else {
 
               <p>Like many other qualified mental health clinicians, I have elected not to join any health insurance panels at this time.
 
-              <p>Your health insurance carrier may allow you to choose an out-of-network provider. I am happy to provide you with a summary of charges so that you can seek reimbursement. I advise you to check with your carrier to verify coverage for out-of-network providers and rates of reimbursement.
-
               <p>Please contact me to discuss my fee. I offer a 15 minute phone consultation at no charge. This gives us an opportunity to discover what you are looking for, and determine how I can best serve you. 
 
             <p><b>Office Location:</b><br/>
